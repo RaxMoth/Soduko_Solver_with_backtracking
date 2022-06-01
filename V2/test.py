e88aca330eb4f81678d3ff3995d5a54c32b6ba40
Maximilian Roth <46791650+Snotters@users.noreply.github.com>
2022-06-01 13:07:21 +0200

    Update test.py

--- V2/test.py
+++ V2/test.py
@@ -14,7 +14,6 @@ display = pygame.display.set_mode((300, 300))
 while True:
 	
 	# creating a loop to check events that
-	# are occuring
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
@@ -23,18 +22,14 @@ while True:
 		# checking if keydown event happened or not
 		if event.type == pygame.KEYDOWN:
 			
-			# checking if key "A" was pressed
 			if event.key == pygame.K_0:
 				print("Key 1 has been pressed")
 			
-			# checking if key "J" was pressed
 			if event.key == pygame.K_1:
 				print("Key 1 has been pressed")
 			
-			# checking if key "P" was pressed
 			if event.key == pygame.K_2:
 				print("Key 2 has been pressed")
-			
-			# checking if key "M" was pressed
+				
 			if event.key == pygame.K_3:
 				print("Key 3 has been pressed")
